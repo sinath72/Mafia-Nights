152c5cd63fc7084bcad73d7d937e99733436206e
sina taherkhani <registry123456@gmail.com>
2023-03-13 06:10:21 -0700

    add SearchBox to Let'sPlay bug in tap around set SearcBox for Acts for next time

--- Mafia Nights/ViewModel/Let'sPlay.swift
+++ Mafia Nights/ViewModel/Let'sPlay.swift
@@ -12,6 +12,8 @@ struct LetsPlay: View{
     @State private var checkbox = false
     @State private var checkboxArray = []
     @State private var PlayerName: String = ""
+    @State private var SearchText: String = ""
+    @State private var isFocused:Bool = false
     @State var name:[PlayerModel] = []
     @State var alreadyName:[String] = []
     var body : some View{
@@ -21,24 +23,69 @@ struct LetsPlay: View{
                     .ignoresSafeArea()
                     .overlay(
                         List{
-                            ForEach(0..<name.count,id: \.self){ i in
-                                Toggle(isOn: $name[i].modify) {
-                                    Text(name[i].name.description)
-                                }
-                                .listRowBackground(Color.teal)
-                                .onChange(of: name[i].modify, perform: { value in
-                                    if value{
-                                        alreadyName.append($name[i].name.wrappedValue)
-                                        print(alreadyName)
+                            Color.green
+                                .ignoresSafeArea()
+                                .overlay(
+                                    TextField("Search", text: $SearchText, onEditingChanged: { isChange in
+                                        if $SearchText.wrappedValue == ""{
+                                            isFocused = false
+                                        }
+                                        else{
+                                            isFocused = true
+                                        }
+                                    }).frame(width:345,height:isFocused ? 10:45,alignment:.center)
+                                        .padding(.leading, isFocused ? 25 : 50  )
+                                        .padding(.trailing, isFocused ? 25 : 50 )
+                                        .padding(.top, isFocused ? 20 : 0  )
+                                        .padding(.bottom, isFocused ? 20 : 0 )
+                                        .background(Color.gray)
+                                        .foregroundColor(Color.black)
+                                )
+                            if $SearchText.wrappedValue == "" && isFocused == false{
+                                ForEach(0..<name.count,id: \.self){ i in
+                                    Toggle(isOn: $name[i].modify) {
+                                        Text(name[i].name.description)
                                     }
-                                    else{
-                                        if alreadyName.contains($name[i].name.wrappedValue) {
-                                            let index = alreadyName.firstIndex(of: $name[i].name.wrappedValue)
-                                            alreadyName.remove(at: index!)
+                                    .listRowBackground(Color.teal)
+                                    .onChange(of: name[i].modify, perform: { value in
+                                        if value{
+                                            if alreadyName.contains($name[i].name.wrappedValue) == false {
+                                                alreadyName.append($name[i].name.wrappedValue)
+                                                print(alreadyName)
+                                            }
+                                        }
+                                        else{
+                                            if alreadyName.contains($name[i].name.wrappedValue) {
+                                                let index = alreadyName.firstIndex(of: $name[i].name.wrappedValue)
+                                                alreadyName.remove(at: index!)
+                                                print(alreadyName)
+                                            }
+                                        }
+                                    })
+                                }
+                            }
+                            else{
+                                let filterNames = $name.filter {$0.name.wrappedValue == $SearchText.wrappedValue}
+                                var p = print(filterNames.first?.modify)
+                                ForEach(0..<filterNames.count,id: \.self){ i in
+                                    Toggle(isOn: filterNames[i].modify) {
+                                        Text(filterNames[i].name.wrappedValue)
+                                    }
+                                    .listRowBackground(Color.teal)
+                                    .onChange(of: filterNames[i].modify.wrappedValue, perform: { value in
+                                        if value{
+                                            alreadyName.append(filterNames[i].name.wrappedValue)
                                             print(alreadyName)
                                         }
-                                    }
-                                })
+                                        else{
+                                            if alreadyName.contains(filterNames[i].name.wrappedValue) {
+                                                let index = alreadyName.firstIndex(of: filterNames[i].name.wrappedValue)
+                                                alreadyName.remove(at: index!)
+                                                print(alreadyName)
+                                            }
+                                        }
+                                    })
+                                }
                             }
                         }.scrollContentBackground(.visible)
                             .listStyle(.plain)
